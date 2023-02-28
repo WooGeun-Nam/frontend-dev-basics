f68c271930d1050999c489ed0173eb77f53d31ad
WooGeun-Nam <rolru7686@gmail.com>
2023-02-28 10:11:00 +0900

    ajax #4

--- ch08/src/main/java/ch08/controller/test/ApiTestController.java
+++ ch08/src/main/java/ch08/controller/test/ApiTestController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test")
 public class ApiTestController {
+	@RequestMapping("/text")
+	public String text() {
+		return "text";
+	}
+
 	@RequestMapping("/json01")
 	public String json01() {
 		return "json01";
+	}
+
+	@RequestMapping("/html")
+	public String html() {
+		return "html";
+	}
+
+	@RequestMapping("/xml")
+	public String xml() {
+		return "xml";
 	}
 }
